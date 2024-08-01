8f7780ac633cc65a653f0ca2222f7b6cb82fab27
Kamran Azim <Kamazim121212@gmail.com>
2024-08-01 18:33:51 +0100

    Removed HTML Entities using unescape

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from html import unescape
 
 question_bank = []
 for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
+    question_text = unescape(question["question"])
+    question_answer = unescape(question["correct_answer"])
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
