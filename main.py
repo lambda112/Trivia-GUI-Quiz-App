75a544a95d7aea2fa4118ebc234f20c48a16123e
Kamran Azim <Kamazim121212@gmail.com>
2024-08-02 18:25:55 +0100

    Provide feedback to the plater

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-from html import unescape
 from ui import Interface
 
 question_bank = []
 for question in question_data:
-    question_text = unescape(question["question"])
-    question_answer = unescape(question["correct_answer"])
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
 quiz = QuizBrain(question_bank)
+quiz_interface = Interface(quiz)
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
--- ui.py
+++ ui.py
@@ -1,28 +1,46 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class Interface():
-    def __init__(self) -> None:
+    def __init__(self, quiz_brain: QuizBrain) -> None:
         self.window = Tk()
+        self.question = quiz_brain
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, background = THEME_COLOR)
 
         # Label
-        self.score_text = Label(text = "Score: ", bg = THEME_COLOR, fg = "white", font=("Arial", 12, "bold"))
+        self.score_text = Label(text = f"Score: ", bg = THEME_COLOR, fg = "white", font=("Arial", 12, "bold"))
         self.score_text.grid(row=0, column=1, sticky="ew")
 
         # Canvas
-        canvas = Canvas(height=250, width=300, bg="white")
-        canvas.create_text(150, 125, text = "Some Question Text", fill = "black", font = ("Arial", 20, "italic"))
-        canvas.grid(row=1, column=0, sticky="ew", columnspan=2, pady=50)
+        self.canvas = Canvas(height=250, width=300, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, text = "Some Question Text", fill = "black", font = ("Arial", 15, "italic"), width=280)
+        self.canvas.grid(row=1, column=0, sticky="ew", columnspan=2, pady=50)
 
         # Buttons
         true_img = PhotoImage(file = "images/true.png")
         false_img = PhotoImage(file = "images/false.png")
-        self.true_button = Button(image=true_img, highlightthickness=0).grid(row = 2, column = 0)
-        self.false_button = Button(image=false_img, highlightthickness=0).grid(row = 2, column = 1)
+        self.true_button = Button(image=true_img, highlightthickness=0, command=self.user_true_answer).grid(row = 2, column = 0)
+        self.false_button = Button(image=false_img, highlightthickness=0, command=self.user_false_answer).grid(row = 2, column = 1)
 
+        self.get_next_question()
         self.window.mainloop()
 
-quiz_interface = Interface()
+    def get_next_question(self):
+        self.canvas.config(bg = "white")
+        self.score_text.config(text= f"Score: {self.question.score}")
+        q_text = self.question.next_question()
+        self.canvas.itemconfig(self.question_text, text = q_text)
+
+    def user_true_answer(self):
+        self.give_feedback(self.question.check_answer("True"))
+        
+    def user_false_answer(self):
+        self.give_feedback(self.question.check_answer("False"))
+
+    def give_feedback(self, answer):
+        self.canvas.config(background='green') if answer == True else self.canvas.config(background='red')
+        self.window.after(1000, self.get_next_question)
+        
